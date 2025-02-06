98c521d480a24512dee814842508af4b02538eb3
Josh Hughes <josh.hughes@bionical.com>
2025-02-06 10:14:21 +0000

    feat: adding ufo for folding support

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -41,3 +41,7 @@ vim.opt.shortmess:append("c") -- Don't give |ins-completion-menu| messages (defa
 vim.opt.iskeyword:append("-") -- Hyphenated words recognized by searches (default: does not include '-')
 vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
+vim.o.foldmethod = "manual" -- Let ufo handle folds
+vim.o.foldexpr = "nvim_treesitter#foldexpr()" -- Use Treesitter for fold calculations
+vim.o.foldlevel = 99 -- Ensure folds are expanded by default
+vim.o.foldlevelstart = 99 -- Start with everything expanded
